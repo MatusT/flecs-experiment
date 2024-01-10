2ccd7688862e7b5a0e48a920f4ea372b2b6dfe98
Matus Talcik <matus.talcik@outlook.com>
2024-01-10 01:31:01 +0100

    Use vcpkg

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <flecs.h>
-#include <iostream>
+#include <variant>
 #include <print>
 
 struct StructuralElement {};
+struct Child {};
+
 struct Point : StructuralElement {
     double x;
     double y;
 };
+
+struct PointBetweenTwoPoints : StructuralElement {};
+
+struct PointOnLine : Point {
+    double t;
+};
+
 struct Line : StructuralElement {
     double x0;
     double y0;
@@ -14,23 +23,22 @@ struct Line : StructuralElement {
     double x1;
     double y2;
 };
-struct PointBetweenTwoPoints : StructuralElement {};
-struct PointOnLine : Point {
-    double t;
-};
 
-struct Child {};
+struct Surface {
+    double area;
+};
 
 int main() {
     auto world = flecs::world{};
 
-    world.component<StructuralElement>();
+    const flecs::entity &structuralElementComponent = world.component<StructuralElement>();
+    const flecs::entity &childComponent = world.component<Child>().add(flecs::Traversable);
     const flecs::entity &pointComponent = world.component<Point>().is_a<StructuralElement>();
     const flecs::entity &lineComponent = world.component<Line>().is_a<StructuralElement>();
     const flecs::entity &pointBetweenTwoPointsComponent = world.component<PointBetweenTwoPoints>().is_a<StructuralElement>();
     const flecs::entity &pointOnLineComponent = world.component<PointOnLine>().is_a<StructuralElement>();
 
-    const flecs::entity &childComponent = world.component<Child>().add(flecs::Traversable);
+    flecs::query<> q = world.query_builder().term<StructuralElement>().cascade(childComponent).self().term<Point>().inout(flecs::InOut).or_().term<Line>().build();
 
     const flecs::entity &p1 = world.entity("P1").add<StructuralElement>().set<Point>({.x = 0.0, .y = 0.0});
     const flecs::entity &p2 = world.entity("P2").add<StructuralElement>().set<Point>({.x = 10.0, .y = 0.0});
@@ -42,9 +50,8 @@ int main() {
 
     const flecs::entity &line2 = world.entity("L2").add<StructuralElement>().add<Line>().add<Child>(p3).add<Child>(p4);
 
-    flecs::query<> q = world.query_builder().term<StructuralElement>().cascade(childComponent).self().term<Point>().inout(flecs::InOut).or_().term<Line>().build();
 
-    q.each([&](flecs::iter &it, size_t index) {
+    q.each([&](flecs::iter &it, size_t index, std::variant<Point*, Line*> v) {
         flecs::entity entity = it.entity(index);
         flecs::id term_id = it.id(2);
 
@@ -62,7 +69,7 @@ int main() {
             std::println("Point {0} | X: {1:.3f} Y: {2:.3f}", entity.name().c_str(), v->x, v->y);
         }
         if (term_id == world.id<Line>()) {
-            std::cout << "is a line " << entity.name() << std::endl;
+            //std::cout << "is a line " << entity.name() << std::endl;
 
             flecs::entity from = entity.target(childComponent, 0);
             flecs::entity to = entity.target(childComponent, 1);
